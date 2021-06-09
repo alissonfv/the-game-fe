a063573f81bcf7cfcfae40f2ca3bb619059235c7
Schwaaan <jair.schwan9@gmail.com>
2021-06-09 19:16:39 -0300

    initializing login

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,33 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Button } from 'reactstrap';
-import { InputGroupAddon,  Input } from 'reactstrap';
+import React, { useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import {login1} from '../pages/login/service/LoginService'
+
+export default function LoginUser() {
+
+    const [login, setLogin] = useState('');
+
+    return (
+        <Container>
+            <Form>
+                <Col>
+                    <Row className="mb-4 w-25">
+                        <Form.Control type="text" onChange={e => setLogin(e.target.value)} maxLength={30} placeholder="Login" />
+                    </Row>
+                    <Row className="mb-4 w-25">
+                        <Form.Control type="password" placeholder="Senha" />
+                    </Row>
+                    <Row>
+                        <Col xs="auto" className="my-1">
+                            <Button variant="light" onClick={() => login1(login, null, null)}>Entrar</Button>
+                        </Col>
+                        <Col xs="auto" className="my-1">
+                            <Button variant="light" type="submit">Criar Conta</Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Form>
+        </Container>
+    );
+}
+
+
---
+++ src/pages/login/service/LoginService.js
@@ -0,0 +1,3 @@
+export  function login1(username, password, from) {
+        alert(username);
+}
