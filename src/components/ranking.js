de7e10f03892c7eed16404ecebca99e3cd4de919
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-06-11 01:15:51 -0300

    #4 iniciando o ranking

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -1,7 +1,12 @@
 import { React } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Queue from '../utils/nodeQueue';
 
 export default function Ranking() {
-    return (
-        <p>teste</p>
-    );
+    let rankingQueue = new Queue();
+    const url = 'http://localhost:8060/v1/rankings'
+
+    fetch(url).then(res => {
+        console.log(res.body);
+    });
 }
