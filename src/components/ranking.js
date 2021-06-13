fd7bb5d1dc1db9a665ca7ac17182e95e376a3e65
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-06-13 16:03:02 -0300

    #4 início tela de ranking

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -1,12 +1,42 @@
 import { React } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Queue from '../utils/nodeQueue';
+import { getToken } from '../utils/auth';
 
 export default function Ranking() {
     let rankingQueue = new Queue();
     const url = 'http://localhost:8060/v1/rankings'
+    const token = getToken();
+    const options = {
+        method: 'get',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }
 
-    fetch(url).then(res => {
-        console.log(res.body);
+    fetch(url, options).then(res => {
+        return res.json();
+    }).then(data => {
+        data.forEach(element => {
+            console.log(element);
+            rankingQueue.inserting(element);
+        });
     });
+    
+    console.log(rankingQueue);
+    if (rankingQueue.begin && rankingQueue.end) {
+        while(rankingQueue.next()) {
+            console.log("next", rankingQueue.next());
+            rankingQueue.removing();
+        }
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col><p>Nome</p></Col>
+                <Col><p>Pontos</p></Col>
+            </Row>
+        </Container>
+    );
 }
