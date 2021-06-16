a3260a788a30fbf911a5ff802eccc0c65800fb0f
AndrewsVieira <andrews.a.vieira@gmail.com>
2021-06-15 21:17:27 -0300

    #4 usando o useEfecct e async await

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -1,10 +1,11 @@
-import { React, Component } from 'react';
+import { React, useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { getToken } from '../utils/auth';
 import Queue from '../utils/queue';
 
-export default function Ranking() {
-    let rankingQueue = new Queue();
+export default  function Ranking() {
+
+    const rankingQueue = new Queue();
 
     const url = 'http://localhost:8060/v1/rankings'
     const token = getToken();
@@ -15,22 +16,33 @@ export default function Ranking() {
         }
     }
 
-    fetch(url, options).then(res => {
-        return res.json();
-    }).then(data => {
-        console.log(data);
+    const [queue, setQueue] = useState(new Queue());
+
+    async function setDataInQueue(data){
         data.forEach(element => {
             rankingQueue.inserting(element);
         });
-        console.log(`${rankingQueue.getQuantity()} elementos inseridos`);
-    });
+
+        setQueue(rankingQueue);
+    }
+    
+    useEffect(async () => {
+        let res = await fetch(url, options);
+        let data = await res.json();
+        
+        console.log(data);
+        await setDataInQueue(data);
+
+            console.log(`${rankingQueue.next()} elementos inseridos`);
+   }, []);
+
+
 
     return (
         <Container id="container">
-            <Row>
-                <Col><p>Nome</p></Col>
-                <Col><p>Pontos</p></Col>
-            </Row>
+         { rankingQueue ? 
+         console.log("jair lindo", queue.next()) : console.log("jair gatao")
+        }
         </Container>
     );
 }
