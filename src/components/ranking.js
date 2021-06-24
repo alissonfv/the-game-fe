da85eb86273fe2ff45cb9ca3fdc2ce8eb822c8f1
AndrewsVieira <64992771+AndrewsVieira@users.noreply.github.com>
2021-06-24 00:14:56 -0300

    Update ranking.js

--- src/components/ranking.js
+++ src/components/ranking.js
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import { rankingRequest } from '../pages/ranking/service/rankingService';
 
 
